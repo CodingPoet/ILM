f77943bd4d486cc9f3a7e79b3f14c7d3f417f8c5
Clinton Evans <clint@getctrl.biz>
2015-05-31 18:03:50 +1200

    Converted project to Ubuntu System rather than Windows.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   resources :messages
-
   resources :users
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -12,11 +10,10 @@ Rails.application.routes.draw do
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+  # Example of named route that can be invoked with purchase_url(id:
+  # product.id)   get 'products/:id/purchase' => 'catalog#purchase', as:
+  # :purchase Example resource route (maps HTTP verbs to controller actions
+  # automatically):   resources :products
 
   # Example resource route with options:
   #   resources :products do
